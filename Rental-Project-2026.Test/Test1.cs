d85e395bb87372b112c222fb54b6180a6a3665bb
Samuel Sanchez <samuelsanchez328196correo.itm.edu.co>
2026-03-11 19:35:12 -0500

    Firts Commit

---
+++ Rental-Project-2026.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Rental_Project_2026.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Rental-Project-2026.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rental_Project_2026.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
