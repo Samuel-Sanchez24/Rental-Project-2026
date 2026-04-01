b7ce7795c1621f11eafd572146d676eaa2bf0573
Samuel-Sanchez24 <samuelsanchez328196@correo.itm.edu.co>
2026-04-01 15:16:17 -0500

    Implementacion modulo de sucursales (B + Controller + View)

---
+++ Rental-Project-2026.Web/Controllers/BranchesController.cs
@@ -0,0 +1,60 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Rental_Project_2026.Application.UseCases.Branches.Commands;
+using Rental_Project_2026.Web.DTOs.Branches;
+
+namespace Rental_Project_2026.Web.Controllers
+{
+    public class BranchesController : Controller
+    {
+        private readonly INotyfService _notyfService;
+        private readonly IMediator _mediator;
+
+        public BranchesController(INotyfService notyfService, IMediator mediator)
+        {
+            _notyfService = notyfService;
+            _mediator = mediator;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateBranchDTO dto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    _notyfService.Error("Debe corregir los errores de validacion");
+                    return View(dto);
+                }
+
+                CreateBranchCommand command = new CreateBranchCommand
+                {
+                    Name = dto.Name,
+                    City = dto.City,
+                    Address = dto.Address,
+                    Phone = dto.Phone,
+                    Status = dto.Status
+                };
+
+                Guid NewBranchId = await _mediator.Send(command);
+                _notyfService.Success("Sucurusal creada exitosamente!");
+            }
+            catch (Exception ex)
+            {
+                _notyfService.Error($"Error al crear la sucursal: {ex.Message}");
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Rental-Project-2026.Web/DTOs/Branches/CreateBranchDTO.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rental_Project_2026.Web.DTOs.Branches
+{
+    public class CreateBranchDTO
+    {
+        [Required]
+        [StringLength(64, MinimumLength = 3)]
+        [Display(Name = "Sucursal")]
+        public required string Name { get; set; }
+
+        [Required]
+        [StringLength (64, MinimumLength = 2)]
+        [Display(Name = "Ciudad")]
+        public required string City { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        [Display(Name = "Direccion")]
+        public required string Address { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Telefono")]
+        public required string Phone { get; set; }
+
+        [EnumDataType(typeof(BranchStatus))]
+        [Display(Name = "Estado")]
+        public required BranchStatus Status { get; set; }
+    }
+}
