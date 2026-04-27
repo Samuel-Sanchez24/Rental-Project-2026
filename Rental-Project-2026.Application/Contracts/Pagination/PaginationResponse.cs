ca4628478b19d4a6e86615e196a709ab2bc1a8b9
Abdul Cataño <jhonnier.muox@gmail.com>
2026-04-26 19:32:04 -0500

    Add pagination request and response

---
+++ Rental-Project-2026.Application/Contracts/Pagination/PaginationResponse.cs
@@ -0,0 +1,28 @@
+namespace Rental_Project_2026.Application.Contracts.Pagination
+{
+    public class PaginationResponse<T>
+    {
+        public required List<T> Items { get; init; }
+        public int TotalCount { get; init; }
+        public int PageNumber { get; init; }
+        public int PageSize { get; init; }
+
+        public int TotalPages => PageSize <= 0
+            ? 0
+            : (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+
+        public static PaginationResponse<T> Create(List<T> items, int totalCount, PaginationRequest request)
+        {
+            return new PaginationResponse<T>
+            {
+                Items = items,
+                TotalCount = totalCount,
+                PageNumber = request.PageNumber,
+                PageSize = request.PageSize
+            };
+        }
+    }
+}
